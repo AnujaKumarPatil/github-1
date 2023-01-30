b6ab5fe81035854924fbb07b0e27248a1ba15e09
Admin <Admin@DESKTOP-2UCQV10>
2023-01-30 17:24:11 +0530

    [Anuja] demo  created in project

--- src/sample1/Demo_2.java
+++ src/sample1/Demo_2.java
@@ -1,5 +1,16 @@
 package sample1;
 
 public class Demo_2 {
-
+public static void main(String[] args) {
+	System.out.println("Hii Gm");
+	System.out.println("Hello how are you?");
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
